cc25fb397cc85944755d408342c281ae09a8cec4
thodson <thodson@usgs.gov>
2024-02-21 14:19:13 -0600

    Draft API gateway

---
+++ terraform/aws/api_gateway.tf
@@ -0,0 +1,65 @@
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name        = "ratingcurve-api"
+  description = "API Gateway for rating curve demo"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  #path_part   = "ratingcurve-demo"
+  path_part = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  #http_method   = "POST"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.fit_rating.invoke_arn
+}
+
+# might need to include proxy_root and lambda_root from terrafomr example
+resource "aws_api_gateway_deployment" "fit_rating" {
+  depends_on = [
+    "aws_api_gateway_integration.lambda",
+    #"aws_api_gateway_integration.lambda_root",
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  stage_name  = "test"
+}
+
+# everything below this line was taken from an older example,
+# and might not be relevant 
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = aws_api_gateway_method_response.proxy.status_code
+
+  depends_on = [
+    aws_api_gateway_method.proxy,
+    aws_api_gateway_integration.lambda_integration
+  ]
+}
